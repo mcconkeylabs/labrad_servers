5fec4fa2170daf09655957dcb2013a9f007d60e7
Unknown <mcconkey.labs@gmail.com>
2017-09-06 16:03:44 -0400

    Added Definitions
    Defined directories and paths for externals programs.

--- vuv/definitions.py
+++ vuv/definitions.py
@@ -1,3 +1,15 @@
 import os
 
+
+
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+manager_parts = ['scalabrad-0.8.3', 'bin', 'labrad.bat']
+web_parts = ['scalabrad-web-server-2.0.4', 'bin', 'labrad-web.bat']
+def gen_root_path(parts):
+    p = [ROOT_DIR, '..'] + parts
+    return os.path.abspath(os.path.join(p))
+
+MANAGER_SCRIPT = gen_root_path(manager_parts)
+WEB_SCRIPT = gen_root_path(web_parts)
