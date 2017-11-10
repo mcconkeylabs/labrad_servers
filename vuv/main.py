1ed1e7b46bcba1011505d85a86559a3821bd2062
Unknown <mcconkey.labs@gmail.com>
2017-11-10 18:15:13 -0500

    continued gui

--- vuv/main.py
+++ vuv/main.py
@@ -1,2 +1,15 @@
-from vuv.app import startup as SU
+import sys
+import app.subprocs.labrad_procs as LS
+from gui.log.logger import LogWidget
+from PyQt5.QtWidgets import QApplication
 
+if __name__ == '__main__':
+    app = QApplication()
+    
+    procs = [LS.LabradExecutable(),
+             LS.ManagerProcess()]
+    
+    w = LogWidget()
+    w.show()
+    
+    sys.exit(app.exec_())
